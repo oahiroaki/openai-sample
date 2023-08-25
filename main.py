a30ceb9c3f8cbf6574ff2dcbbf743b42b0b431bf
大竹広明 <oahiroaki@gmail.com>
2023-08-25 12:05:21 +0900

    ページ切り替え

--- main.py
+++ main.py
@@ -6,75 +6,89 @@ from dotenv import load_dotenv
 import streamlit as st
 import openai
 
-# .envファイルの読み込み
-load_dotenv()
-# OpenAI APIキーの設定
-openai.api_key = os.environ["OPENAI_API_KEY"]
-
-# Title
-st.title("streamlit sample")
-
-# side
-st.sidebar.title("Options")
-
-# サイドバーにオプションボタンを設置
-user_select_model = st.sidebar.radio("Choose a model:", ["gpt-3.5-turbo"])
-
-# サイドバーにボタンを設置
-clear_button = st.sidebar.button("Clear Conversation", key="clear")
-
-# サイドバーにスライダーを追加し、temperatureを0から2までの範囲で選択可能にする
-# 初期値は0.0、刻み幅は0.1とする
-user_select_temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.1)
-
 # 定数定義
 USER_NAME = "user"
 ASSISTANT_NAME = "assistant"
 
-def response_chatgpt(user_message: str):
+def initialize():
     """
-    ChatGPTのレスポンスを取得
-    Args:
-        user_msg (str): ユーザーメッセージ。
+    初期表示処理
     """
-    chat_response = openai.ChatCompletion.create(
-        model=user_select_model,
-        temperature=user_select_temperature,
-        messages=[
-            {"role": "user", "content": user_message},
-        ],
-        stream=True,
+
+    st.set_page_config(
+        page_title="streamlit sample",
+        page_icon="🤗"
     )
-    return chat_response
+
+    # サイドバー
+    st.sidebar.title("Navigation")
+    # サイドバー：ページ切り替えナビゲーション
+    user_selection = st.sidebar.radio("Go to", ["Chat", "PDF Upload", "Ask My PDF(s)"])
+
+    if user_selection == "PDF Upload":
+        page_pdf_upload_and_build_vector_db()
+    elif user_selection == "Ask My PDF(s)":
+        page_ask_my_pdf()
+    else:
+        page_chat()
 
 
-# チャットログを保存したセッション情報を初期化
-if "chat_log" not in st.session_state:
-    st.session_state.chat_log = []
+def page_pdf_upload_and_build_vector_db():
+    """_summary_
+    PDF読み込みページ
+    """
+    return
 
+def page_ask_my_pdf():
+    """_summary_
+    PDF問い合わせページ
+    """
+    return
 
-user_msg = st.chat_input("ここにメッセージを入力")
+def page_chat():
+    """_summary_
+    チャットページ
+    """
 
-if user_msg:
-    # 以前のチャットログを表示
-    for chat in st.session_state.chat_log:
-        with st.chat_message(chat["name"]):
-            st.write(chat["msg"])
+    st.title("Chat")
 
-    # 最新のメッセージを表示
-    with st.chat_message(USER_NAME):
-        st.write(user_msg)
+    # サイドバー：モデル選択
+    user_select_model = st.sidebar.radio("Choose a model:", ["gpt-3.5-turbo"])
 
-    # アシスタントのメッセージを表示
-    response = response_chatgpt(user_msg)
-    with st.chat_message(ASSISTANT_NAME):
-        assistant_response_area = st.empty()
-        assistant_message = ""
-        for chunk in response:
-            # 回答を逐次表示
-            assistant_message += chunk["choices"][0]["delta"].get("content", "")
-            assistant_response_area.write(assistant_message)
+    # サイドバー：会話履歴削除
+    clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
-    # セッションにチャットログを追加
-    st.session_state.chat_log.append({"name": USER_NAME, "msg": user_msg})
-    st.session_state.chat_log.append({"name": ASSISTANT_NAME, "msg": assistant_message})
+    # サイドバーにスライダーを追加し、temperatureを0から2までの範囲で選択可能にする
+    # 初期値は0.0、刻み幅は0.1とする
+    user_select_temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.1)
+
+    if user_message := st.chat_input("ここにメッセージを入力"):
+        # 最新のメッセージを表示
+        with st.chat_message(USER_NAME):
+            st.write(user_message)
+
+        # AI問い合わせ
+        response = openai.ChatCompletion.create(
+            model=user_select_model,
+            temperature=user_select_temperature,
+            messages=[
+                {"role": "user", "content": user_message},
+            ],
+            stream=True,
+        )
+
+        with st.chat_message(ASSISTANT_NAME):
+            assistant_response_area = st.empty()
+            assistant_message = ""
+            for chunk in response:
+                # 回答を逐次表示
+                assistant_message += chunk["choices"][0]["delta"].get("content", "")
+                assistant_response_area.write(assistant_message)
+
+if __name__ == '__main__':
+    # .envファイルの読み込み
+    load_dotenv()
+    # OpenAI APIキーの設定
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    initialize()
